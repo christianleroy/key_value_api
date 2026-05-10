1fa7cf4f4fc771b7205ed084372ece0ddd1e41e7
Christian Leroy Castillo <42598080+christianleroy@users.noreply.github.com>
2026-05-10 19:58:50 +0800

    Fix test

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -1,7 +1,5 @@
 <?php
 
-test('the application returns a successful response', function () {
-    $response = $this->get('/');
-
-    $response->assertStatus(200);
+test('the root route redirects to api documentation', function () {
+    $this->get('/')->assertRedirect('/api/documentation');
 });
